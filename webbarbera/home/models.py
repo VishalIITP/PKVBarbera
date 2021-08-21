6a29291d372011af05113239eb45fe5a5272d9ca
VishalIITP <vishaliitp64@gmail.com>
2021-08-21 10:19:30 +0530

    minor changes

--- webbarbera/home/models.py
+++ webbarbera/home/models.py
@@ -3,27 +3,15 @@
 
 from django.db import models
 from django.db.models.fields import CharField
-# from django.utils import timezone
+
+SERVICECATEGORY_CHOICES = (
+    ("men", "Men"),
+    ("women", "Women"),
+    ("wedding", "Wedding"),
+    ("kids", "Kids"),
 
 
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-
-#     def __str__(self):
-#         return self.question_text
-
-#     def was_published_recently(self):
-#         return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
-
-#     def __str__(self):
-#         return self.choice_text
+)
 
 
 # reviews Models
@@ -43,33 +31,35 @@ class reviewsModel(models.Model):
 class barberaServices(models.Model):
     ServiceID = models.AutoField(primary_key=True)
     serviceName = models.CharField(max_length=50, default="")
-    serviceImage = models.ImageField(upload_to="home/images", height_field=None, width_field=None, )
-    serviceCategory = models.CharField(max_length=50, default="0")
+    serviceImage = models.ImageField(
+        upload_to="home/images", height_field=None, width_field=None, )
+
+    serviceCategory = models.CharField(max_length=50,
+                                       choices=SERVICECATEGORY_CHOICES,
+                                       default="men"
+                                       )
+
+   
     serviceCategoryDiscount = models.IntegerField(default="0")
-    
-    serviceSubCategory1 = models.CharField(max_length=50, default="")
-    serviceSubCategory1Price = models.IntegerField( default="0")
+
+    serviceSubCategory1 = models.CharField(max_length=50, default="Platinum")
+    serviceSubCategory1Price = models.IntegerField(default="0")
     serviceSubCategory1Discount = models.IntegerField(default="0")
-    serviceSubCategory1Desc = models.CharField(max_length=300, default="Barbera")
+    serviceSubCategory1Desc = models.CharField(
+        max_length=300, default="Barbera Platinum")
 
-
-    serviceSubCategory2 = models.CharField(max_length=50, default="")
+    serviceSubCategory2 = models.CharField(max_length=50, default="Gold")
     serviceSubCategory2Price = models.IntegerField(default="0")
     serviceSubCategory2Discount = models.IntegerField(default="0")
-    serviceSubCategory2Desc = models.CharField(max_length=300, default="Barbera")
-    
-    
-   
-    serviceSubCategory3 = models.CharField(max_length=50, default="")
+    serviceSubCategory2Desc = models.CharField(
+        max_length=300, default="Barbera Gold")
+
+    serviceSubCategory3 = models.CharField(max_length=50, default="Silver")
     serviceSubCategory3Price = models.IntegerField(default="0")
     serviceSubCategory3Discount = models.IntegerField(default="0")
-    serviceSubCategory3Desc = models.CharField(max_length=300, default="Barbera")
-   
-    
-   
-   
-   
-   
+    serviceSubCategory3Desc = models.CharField(
+        max_length=300, default="Barbera Silver")
+
     serviceUploadTime = models.DateTimeField()
 
     def __str__(self):
